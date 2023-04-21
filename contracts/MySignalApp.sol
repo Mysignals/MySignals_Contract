8667c0701002eb2ce2c273910adaaa3003b13dbc
Godand <aezeigwe2003@gmail.com>
2023-04-21 12:54:00 +0000

    provider and registrar state variables|set up constructor

--- contracts/MySignalApp.sol
+++ contracts/MySignalApp.sol
@@ -1,8 +1,29 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.18;
 
+error MySignalApp__NotProvider();
+
 contract MySignalApp {
-    constructor() {
-        
+
+
+    uint256 public s_registrarBalance;
+    uint256 public s_fee;
+
+    address public s_registrar;
+
+    mapping (address=>bool)public s_validProvider;
+    mapping (address=>uint256)public s_providerBalance;
+
+    modifier onlyProvider(){
+
+        if (s_validProvider[msg.sender]){
+            _;
+        }
+        revert MySignalApp__NotProvider();
+    }
+
+    constructor(address _registrar, uint256 _fee) {
+        s_registrar=_registrar;
+        s_fee=_fee;
     }
 }
